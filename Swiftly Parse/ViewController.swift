ad6c73be905970eb1cdaa919dbd48d12907f2da6
schwaebek <schwaebek@Katlyns-MacBook-Pro.local>
2014-08-26 09:09:50 -0400

    Parsing in Swift- Initial Commit

---
+++ Swiftly Parse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftly Parse
+//
+//  Created by Katlyn Schwaebe on 8/26/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
